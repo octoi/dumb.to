02abeb22c831cae220ade7634dcb7b0d751641f4
fadhilsaheer <fadhilsaheer@gmail.com>
2022-04-14 13:47:10 +0530

    register form

---
+++ components/register/index.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { Paths } from '@/utils/paths';
+import { Button, Flex, Input } from '@chakra-ui/react';
+
+export const RegisterForm: React.FC = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleFormSubmit = (e: any) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <Flex direction='column'>
+        <form onSubmit={handleFormSubmit}>
+          <h2 className='text-3xl font-bold mb-5'>Register</h2>
+          <Input
+            type='text'
+            variant='filled'
+            placeholder='Name'
+            size='lg'
+            mb={4}
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            disabled={loading}
+          />
+          <Input
+            type='email'
+            variant='filled'
+            placeholder='Email'
+            size='lg'
+            mb={4}
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            disabled={loading}
+          />
+          <Input
+            type='password'
+            variant='filled'
+            placeholder='Password'
+            size='lg'
+            mb={4}
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            disabled={loading}
+          />
+          <Button
+            type='submit'
+            isLoading={loading}
+            size='lg'
+            w='full'
+            colorScheme='teal'
+            mb={4}
+          >
+            Create an account
+          </Button>
+          <p className='text-app-text2'>
+            Already have an account ?{' '}
+            <Link href={Paths.login} passHref>
+              <span className='font-medium cursor-pointer hover:text-app-text1 underline'>
+                Login
+              </span>
+            </Link>
+          </p>
+        </form>
+      </Flex>
+    </div>
+  );
+};
