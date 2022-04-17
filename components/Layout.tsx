c1c003a0aa2c038d30355f5ed38199abe6b35ff9
fadhilsaheer <fadhilsaheer@gmail.com>
2022-04-17 12:47:58 +0530

    more flexible layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import { ReactComponent } from '@/utils/reactProps';
+import { ReactComponent } from '@/utils/reactTypes';
 import { Header } from './header';
 import { Footer } from './Footer';
 import { Container } from '@chakra-ui/react';
@@ -17,6 +17,17 @@ export const Layout: ReactComponent<Props> = ({
   description,
   image,
 }) => {
+  return (
+    <>
+      <LayoutHead title={title} description={description} image={image} />
+      <Header />
+      <Container maxW='container.xl'>{children}</Container>
+      <Footer />
+    </>
+  );
+};
+
+export const LayoutHead: React.FC<Props> = ({ title, description, image }) => {
   title = title || 'DUMB Community 🤯';
   description =
     description ||
@@ -24,19 +35,14 @@ export const Layout: ReactComponent<Props> = ({
   image = image || '/dumb.svg';
 
   return (
-    <>
-      <Head>
-        <title>{title}</title>
-        <link rel='icon' href={image} />
-        <meta name='description' content={description} />
-        <meta property='og:title' content={title} />
-        <meta property='og:description' content={description} />
-        <meta property='og:image' content={image} />
-        <meta property='og:type' content='website' />
-      </Head>
-      <Header />
-      <Container maxW='container.xl'>{children}</Container>
-      <Footer />
-    </>
+    <Head>
+      <title>{title}</title>
+      <link rel='icon' href={image} />
+      <meta name='description' content={description} />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      <meta property='og:image' content={image} />
+      <meta property='og:type' content='website' />
+    </Head>
   );
 };
